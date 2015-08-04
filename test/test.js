7e2b4cf8654e547863c4d56516f5d9230e12c6fa
Alan Hortz <alanhortz@Alans-MacBook-Pro.local>
2015-08-04 22:11:42 +0200

    Refactor the example

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert;
+
+describe('Array', function(){
+  describe('indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+  /*
+  describe('length', function(){
+  	it('should be aqual to 3 when the array contains 3 items', function() {
+  		assert.equal(3, [1,2,3].length);
+  	});
+  });
+  */
+});
