a46360de0c29ad8da4eabeb227f82abb025687bf
Alan Hortz <alan.hortz@gmail.com>
2015-08-18 11:33:12 +0200

    Add Helene

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! Hello Tom");
+  response.end("Hello World! Hello Helene");
 }
 
 var www = http.createServer(handleRequest);
