de99f5a889e7b55b0a1234cb784597e6bea31e52
Alan Hortz <alan.hortz@gmail.com>
2015-08-18 16:20:40 +0200

    Add Tom

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! Hello Helene");
+  response.end("Hello World! Hello Tom Bocken");
 }
 
 var www = http.createServer(handleRequest);
