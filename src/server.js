8b32b420ff283794fdf64d80875e685e721ed5fc
Alan Hortz <alan.hortz@gmail.com>
2015-08-20 13:27:40 +0200

    Remove any greetings !

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! Hello Reza !");
+  response.end("Hello World!");
 }
 
 var www = http.createServer(handleRequest);
