a2e4d1b6c69b9a08bd667e30630b63c38a4160fd
Alan Hortz <alan.hortz@gmail.com>
2015-08-20 13:00:46 +0200

    Add Reza s greetings

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World! Hello Tom Bocken");
+  response.end("Hello World! Hello Reza !");
 }
 
 var www = http.createServer(handleRequest);
